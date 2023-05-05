554dd2986c7266b45c842f6d72ae62e6510b1fe8
Daoquangthiet <thietdq1711@gmail.com>
2023-05-06 00:05:03 +0700

    done

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,5 +1,5 @@
-import { Close, Code } from "@mui/icons-material";
-import { Grid, IconButton } from "@mui/material";
+import { AccountCircle, Close, Code } from "@mui/icons-material";
+import { IconButton, Menu, MenuItem } from "@mui/material";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
@@ -11,7 +11,9 @@ import Typography from "@mui/material/Typography";
 import { makeStyles } from "@mui/styles";
 import Login from "features/Auth/components/Login";
 import Register from "features/Auth/components/Register";
+import { logout } from "features/Auth/userSlice";
 import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { Link, NavLink } from "react-router-dom";
 
 const useStyle = makeStyles({
@@ -22,7 +24,6 @@ const useStyle = makeStyles({
   closeButton: {
     position: "absolute !important",
     top: 1,
-
     right: 1,
     zIndex: 1,
   },
@@ -33,8 +34,13 @@ const MODE = {
 };
 
 export default function Header() {
+  const loggedInUser = useSelector((state) => state.user.current);
+  const isLoggedIn = !!loggedInUser.id;
+  const dispath = useDispatch();
   const [open, setOpen] = useState(false);
   const [mode, setMode] = useState(MODE.LOGIN);
+  const [anchorEl, setAnchorEl] = useState(null);
+  const openMenu = Boolean(anchorEl);
 
   const handleClickOpen = () => {
     setOpen(true);
@@ -44,6 +50,17 @@ export default function Header() {
     if (reason !== "backdropClick" && reason !== "escapeKeyDown") {
       setOpen(false);
     }
+  };
+  const hanleUserClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleCloseMenu = () => {
+    setAnchorEl(null);
+  };
+  const handleLogoutClick = () => {
+    const action = logout();
+    dispath(action);
+    setAnchorEl(null);
   };
 
   const style = useStyle();
@@ -63,11 +80,34 @@ export default function Header() {
           <NavLink className={style.link} to="Album">
             <Button color="inherit">Albums</Button>
           </NavLink>
-          <Button color="inherit" onClick={handleClickOpen}>
-            Register
-          </Button>
+          {!isLoggedIn ? (
+            <Button color="inherit" onClick={handleClickOpen}>
+              Login
+            </Button>
+          ) : (
+            <IconButton
+              color="inherit"
+              aria-controls={openMenu ? "basic-menu" : undefined}
+              aria-haspopup="true"
+              aria-expanded={openMenu ? "true" : undefined}
+              onClick={hanleUserClick}
+            >
+              <AccountCircle />
+            </IconButton>
+          )}
         </Toolbar>
       </AppBar>
+      <Menu
+        anchorEl={anchorEl}
+        open={openMenu}
+        onClose={handleCloseMenu}
+        MenuListProps={{
+          "aria-labelledby": "basic-button",
+        }}
+      >
+        <MenuItem onClick={handleCloseMenu}>My account</MenuItem>
+        <MenuItem onClick={handleLogoutClick}>Logout</MenuItem>
+      </Menu>
       <Dialog open={open} onClose={handleClose}>
         <IconButton className={style.closeButton} onClick={handleClose}>
           <Close />
--- src/features/Auth/userSlice.js
+++ src/features/Auth/userSlice.js
@@ -1,27 +1,36 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import userApi from "api/userApi";
+import StorageKeys from "constants/storages-key";
 
 export const register = createAsyncThunk("user/register", async (payload) => {
   const data = await userApi.register(payload);
   //save to localstorage
-  localStorage.setItem("access_token", data.jwt);
-  localStorage.setItem("user", JSON.stringify(data.user));
+  localStorage.setItem(StorageKeys.TOKEN, data.jwt);
+  localStorage.setItem(StorageKeys.USER, JSON.stringify(data.user));
   return data.user;
 });
 export const login = createAsyncThunk("user/login", async (payload) => {
   const data = await userApi.login(payload);
   //save to localstorage
-  localStorage.setItem("access_token", data.jwt);
-  localStorage.setItem("user", JSON.stringify(data.user));
+  localStorage.setItem(StorageKeys.TOKEN, data.jwt);
+  localStorage.setItem(StorageKeys.USER, JSON.stringify(data.user));
   return data.user;
 });
 const userSlice = createSlice({
   name: "user",
   initialState: {
-    current: {},
+    current: JSON.parse(localStorage.getItem(StorageKeys.USER)) || {},
     settings: {},
   },
-  reducers: {},
+  reducers: {
+    logout(state) {
+      // clear localstorage
+      localStorage.removeItem(StorageKeys.TOKEN);
+      localStorage.removeItem(StorageKeys.USER);
+
+      state.current = {};
+    },
+  },
   extraReducers: (builder) => {
     builder.addCase(register.fulfilled, (state, action) => {
       state.current = action.payload;
@@ -32,5 +41,6 @@ const userSlice = createSlice({
   },
 });
 
-const { reducer } = userSlice;
+const { actions, reducer } = userSlice;
+export const { logout } = actions;
 export default reducer;
